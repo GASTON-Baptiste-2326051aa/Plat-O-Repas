6e07418cda29a3823e4a18acf843f0200058fc99
Pierre-André RIGAT <rigatpierreandre@gmail.com>
2025-03-27 16:42:35 +0100

    Add data.json and update index.js with calendar functionality

--- app/(tabs)/Calendar.js
+++ app/(tabs)/Calendar.js
@@ -4,19 +4,15 @@ import {calendar, index} from "../../assets/style/style.js";
 
 export function Calendar({ days }) {
     return (
-        <View style={index.container}>
+        <View style={calendar.container}>
             {days.map((day, index) => (
                 <View key={index} style={calendar.dayContainer}>
                     <Text style={calendar.dateText}>{day.date}</Text>
-                    {day.plats.length > 0 ? (
-                        day.plats.map((plat, platIndex) => (
-                            <Text key={platIndex} style={calendar.platText}>
-                                {plat.name}
-                            </Text>
-                        ))
-                    ) : (
-                        <Text style={calendar.platText}>Aucun plat disponible</Text>
-                    )}
+                    {day.plats.map((plat, platIndex) => (
+                        <Text key={platIndex} style={calendar.platText}>
+                            {plat.name}
+                        </Text>
+                    ))}
                 </View>
             ))}
         </View>
